6d6131c3c3515ba5a62a1372fd81cd414a1a220a
Elliot Miller <elliot@bitoffdev.com>
2023-03-07 20:20:27 -0500

    Add GPX data from all my hikes

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+import GPX from 'ol/format/GPX';
 import KML from "ol/format/KML";
 import Map from "ol/Map";
 import VectorSource from "ol/source/Vector";
 import View from "ol/View";
 import GeoJSON from "ol/format/GeoJSON";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import {Stroke, Style} from 'ol/style.js';
+
+const routeStyle = new Style({
+  stroke: new Stroke({
+    color: '#eb7d34',
+    width: 2,
+  }),
+});
 
 const backgroundVector = new VectorLayer({
   source: new VectorSource({
@@ -19,8 +28,42 @@ const vector = new VectorLayer({
   }),
 });
 
+const hikeGpxUrls = [
+  "data/hikes/2022-06-04_moosilauke.gpx",
+  "data/hikes/2022-06-19_whiteface_passaconaway.gpx",
+  "data/hikes/2022-06-25_moriah_north_middle_south_carter.gpx",
+  "data/hikes/2022-07-24_tecumseh.gpx",
+  "data/hikes/2022-07-30_flume_liberty.gpx",
+  "data/hikes/2022-08-23_cannon.gpx",
+  "data/hikes/2022-09-03_madison.gpx",
+  "data/hikes/2022-09-11_tom_field_willey_avalon.gpx",
+  "data/hikes/2022-10-02_osceola.gpx",
+  "data/hikes/2022-10-10_haystack_lincoln_lafayette.gpx",
+  "data/hikes/2022-10-15_starr_king_waumbek.gpx",
+  "data/hikes/2022-10-22_hight_carter_dome_wildcat.gpx",
+  "data/hikes/2023-02-12_north_and_south_kinsman.gpx",
+  "data/hikes/2023-03-05_mount_pierce.gpx",
+];
+
+const hikeVectors = hikeGpxUrls.map(
+  url => new VectorLayer({
+    source: new VectorSource({
+      url: url,
+      format: new GPX(),
+    }),
+    style: routeStyle,
+  })
+);
+
+// const pierceVector = new VectorLayer({
+//   source: new VectorSource({
+//     url: "data/hikes/2023-03-05_mount_pierce.gpx",
+//     format: new GPX(),
+//   }),
+// });
+
 const map = new Map({
-  layers: [backgroundVector, vector],
+  layers: [backgroundVector, ...hikeVectors, vector],
   target: document.getElementById("map"),
   view: new View({
     center: [/*x=*/ -7950000, 5500000],
